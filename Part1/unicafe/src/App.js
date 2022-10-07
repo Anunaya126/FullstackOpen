eba10c9b05a0e339b110772acd71f159ee024752
Anunaya Rimal` <Rimalanunaya@gmail.com>
2022-10-07 15:46:11 +0530

    unicafe step 4

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -4,46 +4,59 @@ const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 )
 
-const Display = props=> <div>{props.text}:{props.val}</div>
+const Statistics = (props) => {
+  const calcTotal = (props.good + props.bad + props.neutral)
+  const calcAvg = (calcTotal / 3)
+  const positive = (props.good / calcTotal) * 100 + "%"
 
+  if (props.good===0 && props.bad===0 && props.neutral===0){
+    return(
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+  return(
+  <div>
+    <p>good:{props.good}</p>
+    <p>bad:{props.bad}</p>
+    <p>neutral:{props.neutral}</p>
+    <p>Total:{calcTotal}</p>
+    <p>Average:{calcAvg}</p>
+    <p>Positive:{positive}</p>
+  </div>
+  )
+}
 
 function App() {
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)  
-  
+  const [bad, setBad] = useState(0)
+
   const handleGoodClicks = () => {
-    setGood(good +1)
+    setGood(good + 1)
   }
   const handleNeutralClicks = () => {
-    setNeutral(neutral+1)
+    setNeutral(neutral + 1)
   }
   const handleBadClicks = () => {
-    setBad(bad+1)
+    setBad(bad + 1)
   }
 
-  const calcTotal = (good+bad+neutral)
-  const calcAvg = (calcTotal/3)
-  const positive = (good/calcTotal)*100
-
-
+  
 
   return (
     <div>
       <h1>give feedback</h1>
-      <br/>
+      <br />
       <Button handleClick={handleGoodClicks} text='good' />
       <Button handleClick={handleNeutralClicks} text='neutral' />
       <Button handleClick={handleBadClicks} text='Bad' />
       <h2>Statistics</h2>
-      <br/>
-      <Display val={good} text='good'/>
-      <Display val={neutral} text='neutral'/>
-      <Display val={bad} text='bad'/>
-      <Display val={calcTotal} text='Total'/>
-      <Display val={calcAvg} text='Average'/>
-      <Display val ={positive} text='Positive'/>
+      <br />
+      <Statistics good={good} bad={bad} neutral={neutral}/>
+      
     </div>
   );
 }
